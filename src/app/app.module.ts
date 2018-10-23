96f274318797f8c4a97e7d8ebacc32b5a9ccc01b
saul9402 <saul_harry_@hotmail.com>
2018-10-23 12:15:18 -0500

    Ejemplo de servicio
    Se agrega un mensaje al servicio y se trae a un componente para depsues ser mostrado en el html raiz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProveedoresService } from  './servicios/proveedores.service';
+import { ProveedoresComponent } from './proveedores/proveedores/proveedores.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProveedoresComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ProveedoresService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/servicios/proveedores.service.ts
+++ src/app/servicios/proveedores.service.ts
@@ -6,4 +6,8 @@ import { Injectable } from '@angular/core';
 export class ProveedoresService {
 
   constructor() { }
+
+  getProveedores(){
+    return 'Mensaje desde el servicio';
+  }
 }
