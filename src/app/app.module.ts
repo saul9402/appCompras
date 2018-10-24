a1f1d18e30ad7cb91c58366f797e7a4e8138b670
saul9402 <saul_harry_@hotmail.com>
2018-10-23 22:59:39 -0500

    Agrega Routing
    En este commit se agrega la barra de navegacion que tiene, por ahora, dos links que se activan y desactivan conforme se usan. ademas se usa routing para evitar que las paginas se carguen cada vez que se mueve entre ellas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { ProveedoresService } from  './servicios/proveedores.service';
 import { ProveedoresComponent } from './proveedores/proveedores/proveedores.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { HeaderComponent } from './header/header.component';
+
+  const routes: Routes =[
+    {path: '', component: InicioComponent},
+    {path: 'proveedores', component: ProveedoresComponent},
+    {path: '**', component: InicioComponent}
+  ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProveedoresComponent
+    ProveedoresComponent,
+    InicioComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ProveedoresService],
   bootstrap: [AppComponent]
