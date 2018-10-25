ac0781c16c23452ae83aaf16b4d4ae24f73d327e
saul9402 <saul_harry_@hotmail.com>
2018-10-25 11:37:09 -0500

    Se agrega lista
    Una lista que contiene las provincias y se agrega desde el componente ára que el html lo renderice con una directiva llamada *ngFor

--- src/app/proveedores/addprovee/addprovee.component.ts
+++ src/app/proveedores/addprovee/addprovee.component.ts
@@ -11,6 +11,13 @@ export class AddproveeComponent implements OnInit {
   //Con esto se usan los componente de angular para recibir el objeto del formulario. Se usa el id del html para enlazar el formulario con este archivo
   @ViewChild('formpro') formpro: NgForm;
   proveedor: any;
+  provincias: string[] = [ 'Álava','Albacete','Alicante','Almería','Asturias','Ávila','Badajoz',
+  'Barcelona','Burgos', 'Cáceres', 'Cádiz','Cantabria','Castellón','Ciudad Real','Córdoba',
+  'La Coruña','Cuenca','Gerona','Granada','Guadalajara', 'Guipúzcoa','Huelva','Huesca',
+  'IslasBaleares','Jaén','León','Lérida','Lugo','Madrid', 'Málaga','Murcia','Navarra','Orense',
+  'Palencia','Las Palmas','Pontevedra','La Rioja','Salamanca','Segovia','Sevilla','Soria',
+  'Tarragona','Santa Cruz de Tenerife','Teruel','Toledo','Valencia','Valladolid','Vizcaya',
+  'Zamora','Zaragoza' ]
 
   constructor() { 
   this.proveedor = {
@@ -25,6 +32,7 @@ export class AddproveeComponent implements OnInit {
     contacto: ''}
   }
 
+
   ngOnInit() {
   }
 
@@ -34,6 +42,7 @@ export class AddproveeComponent implements OnInit {
     this.proveedor.direccion = this.formpro.value.direccion;
     this.proveedor.cp = this.formpro.value.cp;
     this.proveedor.localidad = this.formpro.value.localidad;
+    this.proveedor.provincia = this.formpro.value.provincia;
     this.proveedor.telefono = this.formpro.value.telefono;
     this.proveedor.email = this.formpro.value.email;
     this.proveedor.contacto = this.formpro.value.contacto;
