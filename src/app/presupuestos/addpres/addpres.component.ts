1709bb3c02f8dc0e453c510e8e38c4aced011237
saul9402 <saul_harry_@hotmail.com>
2018-10-30 11:49:05 -0600

    Enlaza propiedades
    Se enlazan las propiedades del html con las propiedades del componente para poder manipualr datos desde ahí

--- src/app/presupuestos/addpres/addpres.component.ts
+++ src/app/presupuestos/addpres/addpres.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-addpres',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddpresComponent implements OnInit {
 
-  constructor() { }
+  presupuestoForm: FormGroup;
+  presupuesto: any;
+ 
+ 
+  constructor(private pf: FormBuilder) { }
 
   ngOnInit() {
+    this.presupuestoForm = this.pf.group({
+      proveedor: "",
+      fecha: "",
+      concepto: "",
+      base: "",
+      tipo: '',
+      iva: "",
+      total: ""
+    });
+  }
+
+  onSubmit(){
+    this.presupuesto = this.savePresupuesto();
+  }
+  savePresupuesto(){
+    const savePresupuesto = {
+      proveedor: this.presupuestoForm.get('proveedor').value,
+      fecha: this.presupuestoForm.get('fecha').value,
+      concepto: this.presupuestoForm.get('concepto').value,
+      base: this.presupuestoForm.get('base').value,
+      tipo: this.presupuestoForm.get('tipo').value,
+      iva: this.presupuestoForm.get('iva').value,
+      total: this.presupuestoForm.get('total').value,
+    }
+    return savePresupuesto;
   }
 
 }
