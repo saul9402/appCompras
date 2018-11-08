1229d6324f07ee4e7d411fc86d4825e7c874ac98
saul9402 <saul_harry_@hotmail.com>
2018-11-08 11:12:59 -0600

    Spinner
    Se agrega un sprinner para que al cargar la tabla de proveedores se vea que está haciendo algo, xD

--- src/app/proveedores/proveedores/proveedores.component.ts
+++ src/app/proveedores/proveedores/proveedores.component.ts
@@ -9,6 +9,7 @@ import { ProveedoresService } from '../../servicios/proveedores.service';
 export class ProveedoresComponent implements OnInit {
 
   proveedores: any[] = [];
+  cargando = true;
 
   constructor(private proveedoresService: ProveedoresService) {
     this.proveedoresService.getProveedores()
@@ -21,6 +22,7 @@ export class ProveedoresComponent implements OnInit {
         p.id$ = id$;
         this.proveedores.push(proveedores[id$]);
       }
+      this.cargando = false;
     });
    }
 
