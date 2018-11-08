aed3831b75bb196730d39c4e56407fa043cc7429
saul9402 <saul_harry_@hotmail.com>
2018-11-08 11:31:19 -0600

    Autenticando...
    Se agrega funcionalidad para experiencia de usuario en la autenticacion

--- src/app/autenticacion/inises/inises.component.ts
+++ src/app/autenticacion/inises/inises.component.ts
@@ -15,6 +15,8 @@ export class InisesComponent implements OnInit {
   userdata: any;
   mensaje = false;
 
+  autenticando = false;
+
   constructor(private formBuilder: FormBuilder,
     private autenticacionService: AutenticacionService,
     private router: Router,
@@ -32,11 +34,13 @@ export class InisesComponent implements OnInit {
     });
   }
   onSubmit(){
+    this.autenticando = true;
     this.userdata = this.saveUserData();
     this.autenticacionService.inicioSesion(this.userdata);
     setTimeout(() => {
       if(this.isAuth() === false){
         this.mensaje = true;
+        this.autenticando = false;
       }
     }, 2000)
   }
